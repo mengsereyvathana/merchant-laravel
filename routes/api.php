062f35244051dc8e4db5e931851dec0cc205f15f
vathana <mengvatana101@gmail.com>
2023-09-27 11:48:30 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::group (['middleware' => 'auth:sanctum'],function(){
     Route::get('/user_scheme_price_list_by_category',[ListController::class,'user_scheme_price_list_by_category']);
     Route::post('/add_slide',[ListController::class,'add_slide']);
     Route::delete('/delete_slide',[ListController::class,'delete_slide']);
-    Route::post('/update_slide',[ListController::class,'update_slide']);
+    Route::put('/update_slide',[ListController::class,'update_slide']);
 });
 
 Route::get('/show_cart/{pg?}',[ListController::class,'show_cart']);
