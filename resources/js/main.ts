f1323ec79465def4c252c663756cbc7f573f2eaa
vathana <mengvatana101@gmail.com>
2023-11-06 09:54:32 +0700

    updated

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -2,24 +2,28 @@ import "./bootstrap";
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// vuetify
 import "vuetify/styles";
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+// router & store
 import router from "./router";
 import { store } from "./store";
 
+// firebase
 import firebaseConfig from "@/config/firebase.config";
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
 
+//ripple
 import { rippleInit } from "@/directive/index";
 
+// css tailwind & sweetalert
 import "../css/app.css";
 import "sweetalert2/dist/sweetalert2.css";
-
 
 const vuetify = createVuetify({
     components,
